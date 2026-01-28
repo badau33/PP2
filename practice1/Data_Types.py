f5b5b23d680b3cee10a8f21059b611a85e6110be
badau33 <bakdauleeet4@icloud.com>
2026-01-28 21:00:26 +0500

    add practice1

---
+++ practice1/Data_Types.py
@@ -0,0 +1,31 @@
+# Бүтін сан мысалы
+jas = 25 
+tovar_sany = -10 # Теріс сан да бүтін сан түріне жатады
+
+print(type(jas)) # Нәтиже: <class 'int'>
+
+# Бөлшек сан (float) мысалы
+salmak = 65.5
+pi_sany = 3.14
+
+print(type(salmak)) # Нәтиже: <class 'float'>
+
+# Мәтіндік тип (string) мысалы
+aty = "Айдос"
+soylem = 'Python тілін үйрену өте қызық!'
+
+print(type(aty)) # Нәтиже: <class 'str'>
+
+# Логикалық (boolean) тип мысалы
+okishi_ma = True
+sharlagan_ba = False
+
+print(type(okishi_ma)) # Нәтиже: <class 'bool'>
+
+# Тізім (list) мысалы
+zhemister = ["алма", "банан", "шие"]
+sandar = [1, 2, 3, 4, 5]
+
+print(type(zhemister)) # Нәтиже: <class 'list'>
+# Тізімнің бірінші элементін алу (санау 0-ден басталады)
+print(zhemister[0]) # Нәтиже: алма
